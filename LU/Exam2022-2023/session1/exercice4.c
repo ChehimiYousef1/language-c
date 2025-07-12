8ce3cc6d49d776d4ab3ef876c33c5e8d749c02de
ChehimiYousef1 <122696157+ChehimiYousef1@users.noreply.github.com>
2025-07-12 20:48:58 +0300

    Update exercice4.c

--- LU/Exam2022-2023/session1/exercice4.c
+++ LU/Exam2022-2023/session1/exercice4.c
@@ -1,1 +1,42 @@
+void partition(Node* Head) {
+    int number;
+    printf("Enter the number you want to search for: ");
+    scanf("%d", &number);
 
+    if (Head == NULL) return;
+
+    Node *Current = Head;
+    Node *prev = NULL;
+    Node *next = NULL;
+    int found = 0;
+
+    while (Current != NULL) {
+        if (Current->data == number) {
+            found = 1;
+
+            if (Current->next != NULL && Current->next->data < Current->data) {
+                next = Current->next;
+                Current->next = next->next;
+                next->next = Current;
+
+                if (prev == NULL)
+                    Head = next;
+                else
+                    prev->next = next;
+
+                prev = next;
+            } else {
+                prev = Current;
+                Current = Current->next;
+            }
+        } else {
+            prev = Current;
+            Current = Current->next;
+        }
+    }
+
+    if (found == 0)
+        printf("We can't find\n");
+
+    return;
+}
